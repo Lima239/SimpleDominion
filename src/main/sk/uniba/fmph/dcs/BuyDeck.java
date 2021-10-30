4635fe4d970cd086cb077b973e1d9de7eece1699
Júlia Lichmanová <KlaudiaLichmanova@MacBook-Pro-2.local>
2021-10-30 23:31:20 +0200

    Decks implemented

--- src/main/sk/uniba/fmph/dcs/BuyDeck.java
+++ src/main/sk/uniba/fmph/dcs/BuyDeck.java
@@ -1,2 +1,25 @@
-package sk.uniba.fmph.dcs;public class BuyDeck {
+package sk.uniba.fmph.dcs;
+
+import java.util.NoSuchElementException;
+
+public class BuyDeck {
+    private int count;
+    public GameCardType cardType;
+
+    public BuyDeck(int count, GameCardType cardType){
+        this.count = count;
+        this.cardType = cardType;
+    }
+
+    public int cardCount(){
+        return this.count;
+    }
+
+    public CardInterface buy() {
+        if (count == 0)
+            return null;
+
+        this.count--;
+        return (CardInterface) new Card(this.cardType);
+    }
 }
--- src/main/sk/uniba/fmph/dcs/Deck.java
+++ src/main/sk/uniba/fmph/dcs/Deck.java
@@ -1,2 +1,45 @@
-package sk.uniba.fmph.dcs;public class Deck {
+package sk.uniba.fmph.dcs;
+
+import java.util.*;
+
+import static sk.uniba.fmph.dcs.GameCardType.*;
+
+public class Deck {
+    private List<CardInterface> cards;
+    private int numOfRefills;
+
+    public Deck(){
+        cards = new LinkedList<>();
+        for (int i = 0; i < 7; i++){
+            cards.add(new Card(GAME_CARD_TYPE_COPPER));
+        }
+        for (int i = 0; i < 3; i++){
+            cards.add(new Card(GAME_CARD_TYPE_ESTATE));
+        }
+        numOfRefills = 0;
+    }
+
+    public List<CardInterface> draw(int count){
+        if (cards.size() < count)
+            throw new IndexOutOfBoundsException();
+
+        Collections.shuffle(cards);
+        List<CardInterface> drawn_cards = new ArrayList<CardInterface>();
+        for (int i = 0; i < count; i++) {
+            drawn_cards.add(cards.get(0));
+            cards.remove(0);
+        }
+        return drawn_cards;
+    }
+
+    public void refillFromDiscardPile(DiscardPile pile) {
+        numOfRefills++;
+        this.cards.addAll(pile.shuffle());
+    }
+
+    public int getSize(){
+        return cards.size();
+    }
+    public int getNumOfRefills() { return numOfRefills; }
+    public void removeAll(){ cards.clear(); }
 }
