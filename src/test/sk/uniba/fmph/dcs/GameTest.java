56069da3b6fee8c587761d38b736eaba7caecac8
Júlia Lichmanová <KlaudiaLichmanova@MacBook-Pro-2.local>
2021-10-30 23:29:36 +0200

    Game integration test added

--- src/test/sk/uniba/fmph/dcs/GameTest.java
+++ src/test/sk/uniba/fmph/dcs/GameTest.java
@@ -1,2 +1,24 @@
-package sk.uniba.fmph.dcs;public class GameTest {
+package sk.uniba.fmph.dcs;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+    @Test
+    public void test_game() {
+        EndGameStrategy strategy = new AtLeastNEmptyDecks(3);
+        Game game = new Game(strategy);
+        int idx = game.getIndexOfFirstCardOfType(GameCardType.GAME_CARD_TYPE_FESTIVAL);
+        assertFalse(game.playCard(idx == -1 ? 0 : idx));
+        assertTrue(game.endPlayCardPhase());
+        assertTrue(game.buyCard(1));
+        assertTrue(game.endTurn());
+        assertTrue(game.endPlayCardPhase());
+        assertTrue(game.buyCard(1));
+        assertTrue(game.endTurn());
+        assertTrue(game.endPlayCardPhase());
+        assertFalse(strategy.isGameOver());
+        assertFalse(strategy.isVictory());
+    }
 }
