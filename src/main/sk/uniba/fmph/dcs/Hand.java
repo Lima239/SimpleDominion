06610fc80807fcf2ceff8f0d34a5020870d2abe0
Júlia Lichmanová <KlaudiaLichmanova@MacBook-Pro-2.local>
2021-10-30 23:30:52 +0200

    Play & Hand implemented

--- src/main/sk/uniba/fmph/dcs/Hand.java
+++ src/main/sk/uniba/fmph/dcs/Hand.java
@@ -1,2 +1,58 @@
-package sk.uniba.fmph.dcs;public class Hand {
+package sk.uniba.fmph.dcs;
+
+import java.util.*;
+
+public class Hand {
+    private List<CardInterface> cards;
+    private GameCardType cardType;
+
+    public Hand(Deck deck){
+        this.cards = deck.draw(5);
+    }
+
+    public void addCard(Card card){
+        this.cards.add(card);
+    }
+
+    public void addCards(List<CardInterface> cards){
+        this.cards.addAll(cards);
+    }
+
+    public boolean isActionCard(int idx){
+        return this.cards.get(idx).cardType().isAction();
+    }
+
+    public boolean isInHand(CardInterface card){
+        return this.cards.contains(card);
+    }
+
+    public CardInterface getCard(int idx){
+        return this.cards.get(idx);
+    }
+
+    public CardInterface play(int idx){
+        if (this.cards.get(idx)==null) return null;
+        CardInterface tmp = cards.get(idx);
+        this.cards.remove(idx);
+        return tmp;
+    }
+
+    public List<CardInterface> throwAll(){
+        List<CardInterface> tmp = new ArrayList<>();
+        tmp.addAll(this.cards);
+        this.cards.clear();
+        return tmp;
+    }
+
+    public int getCoinsFromHand(){
+        int countCoins = 0;
+        for (int i = 0; i < this.cards.size(); i++){
+            countCoins += this.cards.get(i).cardType().getPlusCoins();
+        }
+        return countCoins;
+    }
+
+    public int getSize() {
+        return this.cards.size();
+    }
 }
--- src/main/sk/uniba/fmph/dcs/Play.java
+++ src/main/sk/uniba/fmph/dcs/Play.java
@@ -1,2 +1,24 @@
-package sk.uniba.fmph.dcs;public class Play {
+package sk.uniba.fmph.dcs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Play {
+    private ArrayList<CardInterface> cards;
+
+    public Play(){
+        cards = new ArrayList<>();
+    }
+
+    public void putTo(CardInterface card){
+        cards.add(card);
+    }
+
+    public ArrayList<CardInterface> throwAll(){
+        ArrayList<CardInterface> tmpCards = new ArrayList<>();
+        tmpCards.addAll(cards);
+        cards.clear();
+        return tmpCards;
+    }
 }
